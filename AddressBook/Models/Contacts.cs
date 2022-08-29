8e8b024cbb5f00de3f3fe5844d1e3490ebe51ce3
Daniel Westlund <dwestlund531@gmail.com>
2022-08-29 07:26:05 -0500

    initial commit

---
+++ AddressBook/Models/Contacts.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AddressBook.Models
+{
+    public class Contacts
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]  
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        [DisplayName("Phone Number")]
+        public string PhoneNumber { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+    }
+}
